edcaf41056ae90b790a034da46e4c3e9ced06204
Saurabh <gohainsaurabh@gmail.com>
2023-05-12 23:13:18 +0530

    started translation

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+require('dotenv').config()
 
 const app = express();
 app.use(express.json());
@@ -17,7 +18,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: "*",
   },
 });
